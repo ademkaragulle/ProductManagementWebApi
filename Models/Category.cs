8180e7596518816b8ca0d455df29929a6b7edaff
Adem Karagülle <ademkaragulle@outlook.com>
2024-01-04 09:21:14 +0300

    api changed

--- Models/Category.cs
+++ Models/Category.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsStatus { get; set; }
+        public List<Product> Products { get; set; }
     }
 }
---
+++ Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+namespace ProductManagementWebApi.Models
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=(localdb)\MSSQLLocalDB;initial catalog=PMWADb;integrated security=true");
+        }
+        DbSet<Category> Categories { get; set; }
+        DbSet<Product> Products { get; set; }
+    }
+}
